4a383c088727409f1b9ff217710f619c4c6c9ff7
martinmassimo <martin.massimo.91@gmail.com>
2020-12-05 20:46:16 -0300

    configuraciones para persistencia

---
+++ cmd/housesForSale/housesForSale.go
@@ -0,0 +1,54 @@
+package housesForSale
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	"github.com/martinmassimo/Seminario2020GoLang/internal/config"
+	"github.com/martinmassimo/Seminario2020GoLang/internal/database"
+	"github.com/martinmassimo/Seminario2020GoLang/internal/service/chat"
+)
+
+func main() {
+	configFile := flag.String("config", "./config.yaml", "this is the service config")
+	flag.Parse()
+
+	cfg := config.LoadConfig(*configFile)
+
+	db, err := database.NewDatabase(cfg)
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	service, _ := chat.New(db, cfg)
+	httpService := chat.NewHTTPTransport(service)
+
+	r := gin.Default()
+	httpService.Register(r)
+	r.Run()
+}
+
+func createSchema(db *sqlx.DB) error {
+	schema := `CREATE TABLE IF NOT EXISTS messages (
+		id integer primary key autoincrement,
+		text varchar);`
+
+	// execute a query on the server
+	_, err := db.Exec(schema)
+	if err != nil {
+		return err
+	}
+
+	// or, you can use MustExec, which panics on error
+	insertMessage := `INSERT INTO messages (text) VALUES (?)`
+	s := fmt.Sprintf("Message number %v", time.Now().Nanosecond())
+	db.MustExec(insertMessage, s)
+	return nil
+}
